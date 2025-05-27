6950fce5f52962cf84d8c0a8e59159b9eaac62c5
Kboy2000 <ebukaakumuo@gmail.com>
2025-05-27 14:20:57 +0100

    added home

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ mongoose.connect(process.env.REMOTE_DB_URL)
 
 const app= express()
 app.use(express.json())
+app.get("/",(req, res)=>{
+    res.send("Alive")
+})
 app.use('/users', userRoute)
 const server =app.listen(3000, ()=>{
     console.log('connected to server successfully');
