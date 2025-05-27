36e114b9cf1f753ae4b08f963d4b4445378cf8b6
Kboy2000 <ebukaakumuo@gmail.com>
2025-05-27 14:04:37 +0100

    my second commit

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const userRoute= require('./routes/user.route')
+require('dotenv').config()
 
-mongoose.connect("mongodb://localhost:27017/dd")
+mongoose.connect(process.env.REMOTE_DB_URL)
 
 const app= express()
 app.use(express.json())
